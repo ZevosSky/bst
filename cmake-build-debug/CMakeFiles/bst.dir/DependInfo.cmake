8e416a97b8dcc3c7cf7c85fdf4bf5028bb780fcc
ZevosSky <120239831+ZevosSky@users.noreply.github.com>
2024-03-02 22:44:17 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/bst.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/thego/Documents/DigiPen/CS280/BST/bst-map.cpp" "CMakeFiles/bst.dir/bst-map.cpp.o" "gcc" "CMakeFiles/bst.dir/bst-map.cpp.o.d"
+  "/mnt/c/Users/thego/Documents/DigiPen/CS280/BST/driver.cpp" "CMakeFiles/bst.dir/driver.cpp.o" "gcc" "CMakeFiles/bst.dir/driver.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/bst.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bst.dir/bst-map.cpp.o"
+  "CMakeFiles/bst.dir/bst-map.cpp.o.d"
+  "CMakeFiles/bst.dir/driver.cpp.o"
+  "CMakeFiles/bst.dir/driver.cpp.o.d"
+  "bst"
+  "bst.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bst.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
